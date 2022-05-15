bb3edf1a2517f5e709651dddfdef31e2cec2b5f2
thangninh1301 <thang13199@gmail.com>
2022-05-15 23:34:45 +0700

    update terraform

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "public_instance" {
   count           = 3
-  ami             = var.image_id
+  ami             = data.aws_ami.ubuntu.id
   instance_type   = var.instance_type
   subnet_id       = aws_subnet.public_subnet[count.index % length(aws_subnet.public_subnet)].id
-  security_groups = [aws_security_group.private.id]
+  security_groups = [aws_security_group.public.id]
   key_name        = var.key_name
   tags            = {
     Name = var.instance_name[count.index]
@@ -11,7 +11,7 @@ resource "aws_instance" "public_instance" {
 }
 resource "aws_instance" "private_instance" {
   count           = 1
-  ami             = var.image_id
+  ami             = data.aws_ami.ubuntu.id
   instance_type   = var.instance_type
   subnet_id       = aws_subnet.private_subnet[count.index % length(aws_subnet.private_subnet)].id
   security_groups = [aws_security_group.private.id]
---
+++ terraform/nat.tf
@@ -0,0 +1,37 @@
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    "Name" = "thangnd45"
+  }
+}
+
+resource "aws_nat_gateway" "public" {
+  depends_on = [aws_internet_gateway.ig]
+
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet[0].id
+
+  tags = {
+    Name = "Public NAT"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.public.id
+  }
+
+  tags = {
+    "Name" = "private"
+  }
+}
+
+resource "aws_route_table_association" "public_private" {
+  for_each       = { for k, v in aws_subnet.private_subnet : k => v }
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
+}
